9a5ea94942e87bf4bbef9518c8e6cd1b01bb39ca
Justin Braithwaite <justinbraithwaite@gmail.com>
2014-12-27 13:54:02 -0500

    Expose more than just a table

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./lib/table.jsx');
+var Table     = require('./lib/table.jsx');
+var Formatter = require('./lib/formatter.jsx');
+var Cell      = require('./lib/cell.jsx');
+
+module.exports = {
+  Table     : Table,
+  Formatter : Formatter,
+  Cell      : Cell
+};
